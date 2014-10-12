41c7ccb18d58560e1426df25bb3eaa2e39820cd6
Allen Gao <allen001@live.ca>
2014-10-12 00:57:25 -0400

    (v1.0) site deployed, additional improvements made
    -Site has now been deployed with Google App Engine.
    -Characters in the textbox are now HTML-escaped
    -The 'remove' button positioning has been fixed
    -Some additional formatting

--- js/script.js
+++ js/script.js
@@ -11,8 +11,16 @@ $(document).ready(
 		function () {
 			var content = $('input[name=box]').val();
 			var remove = "<span class='remove'>Remove</span>";
+			function escape_html(string) {
+				return string
+				.replace(/&/g, "&amp;")
+				.replace(/</g, "&lt;")
+				.replace(/>/g, "&gt;")
+				.replace(/"/g, "&quot;")
+				.replace(/'/g, "&#039;");
+			}
 			if (content != "") {
-				$('ul').append('<div class="item"><li>' + content + ' ' + remove + '</li></div>');
+				$('ul').append('<div class="item"><li>' + escape_html(content) + '<div class="remove_div">' + remove + '</div></li></div>');
 				$('input').val('');
 			}
 		});
